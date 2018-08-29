83b617378e69c48ca30c7a34d28d067b1741612c
Evgeny Ivanov <busforward@gmail.com>
2018-08-30 01:01:52 +0500

    Finish

--- js/function.js
+++ js/function.js
@@ -33,44 +33,20 @@ $(document).ready(function() {
 		document.documentElement.setAttribute('data-browser', 'not-flex');
 	}
 
-	// First screen full height
-	function setHeiHeight() {
-	    $('.full__height').css({
-	        minHeight: $(window).height() + 'px'
-	    });
-	}
-	setHeiHeight(); // устанавливаем высоту окна при первой загрузке страницы
-	$(window).resize( setHeiHeight ); // обновляем при изменении размеров окна
+	// // First screen full height
+	// function setHeiHeight() {
+	//     $('.full__height').css({
+	//         minHeight: $(window).height() + 'px'
+	//     });
+	// }
+	// setHeiHeight(); // устанавливаем высоту окна при первой загрузке страницы
+	// $(window).resize( setHeiHeight ); // обновляем при изменении размеров окна
 
 
-	// Reset link whte attribute href="#"
 	$('[href*="#"]').click(function(event) {
 		event.preventDefault();
 	});
 
-	// Scroll to ID // Плавный скролл к элементу при нажатии на ссылку. В ссылке указываем ID элемента
-	// $('#main__menu a[href^="#"]').click( function(){ 
-	// 	var scroll_el = $(this).attr('href'); 
-	// 	if ($(scroll_el).length != 0) {
-	// 	$('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
-	// 	}
-	// 	return false;
-	// });
-
-	// Stiky menu // Липкое меню. При прокрутке к элементу #header добавляется класс .stiky который и стилизуем
-    // $(document).ready(function(){
-    //     var HeaderTop = $('#header').offset().top;
-        
-    //     $(window).scroll(function(){
-    //             if( $(window).scrollTop() > HeaderTop ) {
-    //                     $('#header').addClass('stiky');
-    //             } else {
-    //                     $('#header').removeClass('stiky');
-    //             }
-    //     });
-    // });
-   	// setGridMatch($('[data-grid-match] .grid__item'));
-   	// gridMatch();
 
    	$('.lang__current').on('click', function(event) {
    		event.preventDefault();
@@ -89,93 +65,82 @@ $(document).ready(function() {
    	$('select').each(function(index, el) {
    		$(this).select2({
 	   		placeholder: $(this).data('placeholder'),
-		    // allowClear: true,
 		    minimumResultsForSearch: 15
 	   	});
    	});
 
 
     // accordion
-      $('.accordion__toggle').on('click', function() {
+    $('.accordion__toggle').on('click', function() {
 
-          // Создаем переменную для отдельного блока внутри которого находится переключаетель и скрытый текст
-          // Для выбора элемента по которому кликнули используя $(this) (просто не забываем что их может быть много на странице)
-          var item = $(this).closest('.accordion__item');
-          // внутри item ищем скрываемый текст (написал просто для понимания что можно так использовать переменные)
-          var content = item.find('.accordion__content');
+        var item = $(this).closest('.accordion__item');
+        item.toggleClass('open');
 
+    });
 
-          // Убираем класс active у всех переключателей
-          $('.accordion__toggle').not($(this)).removeClass('active');
+    $(".fileupload input[type=file]").change(function(){
+        var filename = $(this).val().replace(/.*\\/, "");
+        $("#filename").text(filename);
+    });
 
-          // Убираем класс open у всех скрываемых контентов
-          $('.accordion__content').removeClass('open');
-
-          // У переключателя проверям наличие класса active
-          if ($(this).hasClass('active')) {
-              // Если класс active присутствует, а значит accordion__content виден, то
-              // Удаляем у нажатого элемента класс active
-              $(this).removeClass('active');
-              // у accordion__content убираем класс open (скрываем контент)
-              content.removeClass('open');
-          } else {
-              // Если у нажатого элемента нет класса active
-              // у элементу по которому кликнули добавляем класс active
-              $(this).addClass('active');
-              // для accordion__content добавляем класс open (показываем контент)
-              content.addClass('open');
-          }
-      });
-
-      $(".fileupload input[type=file]").change(function(){
-           var filename = $(this).val().replace(/.*\\/, "");
-           $("#filename").text(filename);
-      });
-
-      $('.js_multiplecheck').select2({
+    $('.js_multiplecheck').select2({
         closeOnSelect: false
-      })
+    })
 
-      $('.js_multiplecheck').on('select2:open', function() {
+    $('.js_multiplecheck').on('select2:open', function() {
         var container = $('.select2-container').last();
         var wrap = $(this).closest('.js_multiplecheckwrap');
         container.addClass('js_select');
-        console.log('open');
-        
         container.appendTo(wrap);
-      });
+    });
+
+    $('.accordion__toggle_icon').on('click', function(){
+        $(this).closest('.accordion__item').remove();
+    });
+
+    $( "#datepicker" ).datepicker();
+
+    // $('.js_selectfullwidth').on('select2:open', function() {
+    //     var container = $('.select2-container').last();
+    //     var wrap = $(this).closest('.js_selectfullwrap');
+    //     container.addClass('js_selectfull');
+    //     container.appendTo(wrap);
+    // });
+
+    $('.filters__item select').on('select2:open', function() {
+        var container = $('.select2-container').last();
+        var wrap = $(this).closest('.filters__item');
+        container.addClass('filters__selectDrop');
+    });
+
+    var selectLabelClick = false;
+
+    $('.select__label').on('click', function() {
+        var select = $(this).parent().find('select');
+        var selectCont = $(this).parent().find('.select2');
+        $(this).toggleClass('open');
+        var selectLabelClick = true;
+        if ($(this).hasClass('open')) {
+            select.select2('open');
+            // console.log("open")
+        } else {
+            select.select2('close');
+            // console.log("close")
+        }
+        var selectLabelClick = false;
+    });
+
+    $('select').on('select2:close', function() {
+        if (selectLabelClick = false) {
+            $(this).parent().find('.select__label').removeClass('open');
+        }
+    });
+
+    $('select').on('select2:open', function() {
+        if (selectLabelClick = false) {
+            $(this).parent().find('.select__label').addClass('open');
+        }
+    });
 
 });
-    // accordion
-
-// });
-
-$(window).resize(function(event) {
-	checkOnResize()
-});
-
-function checkOnResize() {
-   	// setGridMatch($('[data-grid-match] .grid__item'));
-   	// gridMatch();
-}
-
-function gridMatch() {
-   	$('[data-grid-match] .grid__item').matchHeight({
-   		byRow: true,
-   	});
-}
-
-// function setGridMatch(columns) {
-// 	var tallestcolumn = 0;
-// 	columns.removeAttr('style');
-// 	columns.each( function() {
-// 		currentHeight = $(this).height();
-// 		if(currentHeight > tallestcolumn) {
-// 			tallestcolumn = currentHeight;
-// 		}
-// 	});
-// 	setTimeout(function() {
-// 		columns.css('minHeight', tallestcolumn);
-// 	}, 100);
-// }
 
